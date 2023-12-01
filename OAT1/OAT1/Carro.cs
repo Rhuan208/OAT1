4dfb8323326f8456b8c17fdd15d2c8937ca9b5e1
Rhuan208 <rhuan.alves@sounidesc.com.br>
2023-11-30 21:24:18 -0300

    Oat1

---
+++ OAT1/OAT1/Carro.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAT1
+{
+    internal class Carro
+    {
+        public string marca { get; set; }
+        public int km { get; set; }
+        public int porta { get; set; }
+        public string modelo { get; set; }
+
+
+        private bool emMovimento;
+
+        public Carro()
+        {
+            emMovimento = false;
+        }
+
+        public string Acelerar()
+        {
+            if (emMovimento)
+            {
+                return "O carro já está em movimento!";
+            }
+            else
+            {
+                emMovimento = true;
+                return "O carro está em movimento.";
+            }
+        }
+
+        public string Freiar()
+        {
+            if (emMovimento)
+            {
+                emMovimento = false;
+                return "O carro está parado.";
+            }
+            else
+            {
+                return "O carro já está parado!";
+            }
+        }
+    }
+}
+
+
+
+
---
+++ OAT1/OAT1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAT1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro objcarro = new Carro();
+            objcarro.modelo = "Argo";
+            objcarro.marca = "Fiat";
+            objcarro.porta = 4;
+            objcarro.km = 50000;
+
+
+            Carro objcarro2 = new Carro();
+            objcarro2.modelo = "Gol";
+            objcarro2.marca = "volkswagen";
+            objcarro2.porta = 4;
+            objcarro2.km = 60000;
+
+            Carro objcarro3 = new Carro();
+            {
+                objcarro3.modelo = "Civic";
+                objcarro3.marca = "Honda";
+                objcarro3.porta = 4;
+                objcarro3.km = 70000;
+            };
+            Console.WriteLine(objcarro.modelo);
+            Console.WriteLine(objcarro.marca);
+            Console.WriteLine(objcarro.porta);
+            Console.WriteLine(objcarro.km);
+            Console.WriteLine("*************************");
+            Console.WriteLine(objcarro2.modelo);
+            Console.WriteLine(objcarro2.marca);
+            Console.WriteLine(objcarro2.porta);
+            Console.WriteLine(objcarro2.km);
+            Console.WriteLine("*************************");
+            Console.WriteLine(objcarro3.modelo);
+            Console.WriteLine(objcarro3.marca);
+            Console.WriteLine(objcarro3.porta);
+            Console.WriteLine(objcarro3.km);
+            Console.WriteLine("*************************");
+        }
+
+        private static void Main1()
+        {
+            Carro meuCarro = new Carro();
+
+            Console.WriteLine(meuCarro.Acelerar()); 
+            Console.WriteLine(meuCarro.Freiar());   
+
+            Console.WriteLine(meuCarro.Acelerar());
+            Console.WriteLine(meuCarro.Freiar());   
+
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
+
